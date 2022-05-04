cc101b75b592179abac2c79e074bf99bb0b04dd0
Lev111111 <98773015+ZayaSansanch@users.noreply.github.com>
2022-05-04 15:49:29 +0700

    Add indo and multilanguage (localstorage)

--- Yoga/js/MultiLanguage.js
+++ Yoga/js/MultiLanguage.js
@@ -1,29 +1,63 @@
 console.log("MultiLanguage.js - loaded");
 
-Eng()
+var language = localStorage.getItem("language") || "Eng"
+
+console.info(language)
+
+// localStorage.getItem("language", "Eng")
+
+if (language == "Eng") {
+    Eng()
+} else if (language == "Rus") {
+    Rus()
+} else {
+    console.error("Language unknown: " + language)
+}
 
 // const WinOne = null
+
+function setText(id, text) {
+    var el = document.getElementById(id)
+    if (el) {
+        el.innerHTML = text
+    } else {
+        console.warn("Not Element:", id)
+    }
+}
 
 function Rus() {
     // WinOne = JSON.parse(
     //     '{title: Ега, next: Далее, back: Назад}'
     // );
-    console.log("Language: Rus");
-    document.getElementById("ButtonWinOne").innerHTML = "Далее";
-    document.getElementById("ButtonWinTwo").innerHTML = "Назад";
-    document.getElementById("ButtonInfo").innerHTML = "Инфо";
-    document.getElementById("InfoTitle").innerHTML = "Инфо";
+    console.log("Language: Rus")
+    language = "Rus"
+    localStorage.setItem("language", "Rus")
+    setText("ButtonWinOne", "Далее")
+    setText("ButtonWinOne", "Далее")
+    setText("ButtonWinTwo", "Назад")
+    // setText("ButtonInfo", "Инфо")
+    setText("InfoTitle", "Инфо")
+    // setText("Backk", "Назад")
+    setText("Lan", "Язык:")
+    setText("Eng", "Eng")
+    setText("Rus", "Rus")
 }
 
 function Eng() {
     // WinOne = JSON.parse(
     //     '{title: Yoga, next: Next, back: Back}'
     // );
-    console.log("Language: Eng");
-    document.getElementById("ButtonWinOne").innerHTML = "Next";
-    document.getElementById("ButtonWinTwo").innerHTML = "Back";
-    document.getElementById("ButtonInfo").innerHTML = "Info";
-    document.getElementById("InfoTitle").innerHTML = "Info";
+    console.log("Language: Eng")
+    language = "Rus"
+    localStorage.setItem("language", "Eng")
+    setText("ButtonWinOne", "Next")
+    setText("ButtonWinTwo", "Back")
+    // setText("ButtonInfo", "Info")
+    setText("InfoTitle", "Info")
+    // setText("Backk", "Back")
+    setText("Lan", "Language:")
+    setText("Eng", "Eng")
+    setText("Rus", "Rus")
 }
 
 
--- Yoga/js/clider.js
+++ Yoga/js/clider.js
@@ -1,96 +1,114 @@
+console.log("clider.js - loaded");
+
 var now = 1
 
-    var card1 = document.getElementById("card1")
-    var card2 = document.getElementById("card2")
-    var card3 = document.getElementById("card3")
-    var card4 = document.getElementById("card4")
-    var card5 = document.getElementById("card5")
+var card1 = document.getElementById("card1")
+var card2 = document.getElementById("card2")
+var card3 = document.getElementById("card3")
+var card4 = document.getElementById("card4")
+var card5 = document.getElementById("card5")
 
-    var next = document.getElementById("ButtonWinOne")
-    var back = document.getElementById("ButtonWinTwo")
+var next = document.getElementById("ButtonWinOne")
+var back = document.getElementById("ButtonWinTwo")
 
-    // setInterval(PlusOne(), 1000)
+// setInterval(PlusOne(), 1000)
 
-    back.style.opacity = 0
+back.style.opacity = 0
 
-    function PlusOne() {
-        if (now == 1) {
-            card5.classList.add("vne")
-            back.style.opacity = 0.25
-            next.style.opacity = 0.25
-            back.style.transition = "0.5s"
-            next.style.transition = "0.5s"
-            console.log("clider.js: + " + now)
-        }
-        if (now == 2) {
-            card4.classList.add("vne")
-            back.style.opacity = 0.25
-            next.style.opacity = 0.25
-            next.style.transition = "0.5s"
-            console.log("clider.js: + " + now)
-        }
-        if (now == 3) {
-            card3.classList.add("vne")
-            back.style.opacity = 0.25
-            next.style.opacity = 0.25
-            next.style.transition = "0.5s"
-            console.log("clider.js: + " + now)
-        }
-        if (now == 4) {
-            card2.classList.add("vne")
-            back.style.opacity = 0.25
-            next.style.opacity = 0
-            next.style.transition = "0.5s"
-            console.log("clider.js: + " + now)
-        }
-        if (now == 5) {
-            card5.classList.remove("vne")
-            card4.classList.remove("vne")
-            card3.classList.remove("vne")
-            card2.classList.remove("vne")
-            back.style.opacity = 0
-            console.log("clider.js: 0 " + now)
-            
-            now = 0
-        }
-
-        now++
+function PlusOne() {
+    if (now == 1) {
+        card5.classList.add("vne")
+        card5.classList.remove("normal")
+        back.style.opacity = 0.25
+        next.style.opacity = 0.25
+        back.style.transition = "0.5s"
+        next.style.transition = "0.5s"
+        console.log("clider.js: + " + now)
+    }
+    if (now == 2) {
+        card4.classList.add("vne")
+        card4.classList.remove("normal")
+        back.style.opacity = 0.25
+        next.style.opacity = 0.25
+        next.style.transition = "0.5s"
+        console.log("clider.js: + " + now)
+    }
+    if (now == 3) {
+        card3.classList.add("vne")
+        card3.classList.remove("normal")
+        back.style.opacity = 0.25
+        next.style.opacity = 0.25
+        next.style.transition = "0.5s"
+        console.log("clider.js: + " + now)
+    }
+    if (now == 4) {
+        card2.classList.add("vne")
+        card2.classList.remove("normal")
+        back.style.opacity = 0.25
+        next.style.opacity = 0
+        next.style.transition = "0.5s"
+        console.log("clider.js: + " + now)
+    }
+    if (now == 5) {
+        card5.classList.remove("vne")
+        card5.classList.add("normal")
+        card4.classList.remove("vne")
+        card4.classList.add("normal")
+        card3.classList.remove("vne")
+        card3.classList.add("normal")
+        card2.classList.remove("vne")
+        card2.classList.add("normal")
+        back.style.opacity = 0
+        console.log("clider.js: 0 " + now)
+        
+        now = 0
     }
 
-    function MinsOne() {
-        now--
+    now++
+}
 
-        if (now == 1) {
-            card5.classList.remove("vne")
-            back.style.opacity = 0
-            next.style.opacity = 0.25
-            console.log("clider.js: - " + now)
-        }
-        if (now == 2) {
-            card4.classList.remove("vne")
-            back.style.opacity = 0.25
-            next.style.opacity = 0.25
-            console.log("clider.js: - " + now)
-        }
-        if (now == 3) {
-            card3.classList.remove("vne")
-            back.style.opacity = 0.25
-            next.style.opacity = 0.25
-            console.log("clider.js: - " + now)
-        }
-        if (now == 4) {
-            card2.classList.remove("vne")
-            back.style.opacity = 0.25
-            next.style.opacity = 0.25
-            console.log("clider.js: - " + now)
-        }
-        if (now == 5) {
-            card5.style.transition = "1s"
-            card4.classList.remove("vne")
-            card3.classList.remove("vne")
-            card2.style.transition = "0.5s"
-            console.log("clider.js: 0 " + now)
-            
-            now = 0
-        }
+function MinsOne() {
+    now--
+
+    if (now == 1) {
+        card5.classList.remove("vne")
+        card5.classList.add("normal")
+        back.style.opacity = 0
+        next.style.opacity = 0.25
+        console.log("clider.js: - " + now)
     }
+    if (now == 2) {
+        card4.classList.remove("vne")
+        card4.classList.add("normal")
+        back.style.opacity = 0.25
+        next.style.opacity = 0.25
+        console.log("clider.js: - " + now)
+    }
+    if (now == 3) {
+        card3.classList.remove("vne")
+        card3.classList.add("normal")
+        back.style.opacity = 0.25
+        next.style.opacity = 0.25
+        console.log("clider.js: - " + now)
+    }
+    if (now == 4) {
+        card2.classList.remove("vne")
+        card2.classList.add("normal")
+        back.style.opacity = 0.25
+        next.style.opacity = 0.25
+        console.log("clider.js: - " + now)
+    }
+    if (now == 5) {
+        card5.classList.add("vne")
+        card5.classList.remove("normal")
+        card4.classList.add("vne")
+        card4.classList.remove("normal")
+        card3.classList.add("vne")
+        card3.classList.remove("normal")
+        card2.classList.add("vne")
+        card2.classList.remove("normal")
+        console.log("clider.js: 0 " + now)
+        
+        now = 0
+    }
+}
